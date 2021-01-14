6cd29f62b7902d2cf18a96c9398ed10707113102
Nima Hejazi <nima@robotkudos.com>
2021-01-14 15:58:03 -0800

    Change Jenkinsfile

--- nodeapp/src/app.js
+++ nodeapp/src/app.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const config = require('./config');
 
 
-app.get('/', (req, res) => res.send('Hello World new version!!'));
+app.get('/', (req, res) => res.status(500).send('Hello World new version!!'));
 
 const server = app.listen(config.port, () => console.log(`App is running on port ${config.port}`));
 
