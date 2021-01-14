0086fc111d09b54e8b912308226efea0b583b530
Nima Hejazi <nima@robotkudos.com>
2021-01-14 15:08:57 -0800

    Change Jenkinsfile

--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 docker kill nodeapptest > /dev/null 2>&1
 docker rm nodeapptest > /dev/null 2>&1
 docker rmi nodeapptest > /dev/null 2>&1
-docker build -t nodeapptest -f nodeapp/Dockerfile .
+docker build -t nodeapptest nodeapp/Dockerfile
 docker run --rm -ti nodeapptest npm test
