73cbb0b7380070e0f7b0a9aeb5b26ee317554d8f
Nima Hejazi <nima@robotkudos.com>
2021-01-14 15:50:37 -0800

    Change Jenkinsfile

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ docker kill nodeapp > /dev/null 2>&1
 docker rm nodeapp > /dev/null 2>&1
 docker rmi nodeapp > /dev/null 2>&1
 docker build -t mynodeapp -f nodeapp .
-docker run -p 3000:3000 --name nodeapp mynodeapp
+docker run -p 3000:3000 -e PORT=3000 --name nodeapp mynodeapp
