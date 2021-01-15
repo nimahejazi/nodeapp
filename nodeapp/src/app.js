afdae8c797430ff08873432429adad7d94dc3fde
Nima Hejazi <nima@robotkudos.com>
2021-01-14 19:39:28 -0800

    Another change

--- nodeapp/src/app.js
+++ nodeapp/src/app.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const config = require('./config');
 
 
-app.get('/', (req, res) => res.send('Hello World this is a new version with Melia!!'));
+app.get('/', (req, res) => res.status(400).send('Hello World this is a new version with Melia!!'));
 
 const server = app.listen(config.port, () => console.log(`App is running on port ${config.port}`));
 
